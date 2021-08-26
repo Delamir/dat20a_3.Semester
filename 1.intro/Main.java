e6ea34fcaab4e2a37eb5dd8dfb3e54e724559a62
Delamir <71318139+Delamir@users.noreply.github.com>
2021-08-26 11:27:53 +0200

    Opgave

--- 1.intro/Main.java
+++ 1.intro/Main.java
@@ -2,5 +2,16 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Systemos.out.println("Hej");
+        Systemos.out.println(5);
+
+        System.out.println();
     }
+
+    /*
+    //Sverri løsning
+    public static <T> void out(T t) {
+        System.out.println(t);
+    }
+     */
 }
---
+++ 1.intro/Systemos.java
@@ -0,0 +1,12 @@
+public class Systemos {
+
+    public static class out {
+        public static void println(String s) {
+            System.out.println(s);
+        }
+
+        public static void println(int i) {
+            System.out.println(i);
+        }
+    }
+}
