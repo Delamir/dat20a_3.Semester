d67e397dd7f252c3249ca39d4144900b7f36f083
Delamir <71318139+Delamir@users.noreply.github.com>
2021-09-02 10:28:34 +0200

    Opgave

--- 2_OOP_Recap/OOP_Recap/Main.java
+++ 2_OOP_Recap/OOP_Recap/Main.java
@@ -1,5 +1,14 @@
 package OOP_Recap;
 
+/*
+todo
+Create a zoo/aquarium
+Its possible to name threads in java (Give each animal a unique name and tell who is doing what)
+ASCII animations that move when move is called?
+Animals can die
+Change the activity frequency to your liking
+ */
+
 public class Main {
 
     public static void main(String[] args) {
