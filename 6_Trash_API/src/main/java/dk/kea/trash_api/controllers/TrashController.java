ba1cb651ed60f7cef112ccf6089824e1ff83e5fc
Delamir <71318139+Delamir@users.noreply.github.com>
2021-09-09 11:45:53 +0200

    Spring

--- 6_Trash_API/src/main/java/dk/kea/trash_api/controllers/TrashController.java
+++ 6_Trash_API/src/main/java/dk/kea/trash_api/controllers/TrashController.java
@@ -1,6 +1,7 @@
 package dk.kea.trash_api.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalTime;
 
@@ -15,5 +16,20 @@ public class TrashController {
     @GetMapping("/clock")
     public LocalTime clock() {
         return LocalTime.now();
+    }
+
+    @GetMapping("/sin/{number}")
+    public String sevenDeadlySins(@PathVariable int number) {
+
+        switch(number) {
+            case 1: return "Glottony";
+            case 2: return "Lust";
+            case 3: return "Greed";
+            case 4: return "Wrath";
+            case 5: return "Sloth";
+            case 6: return "Envy";
+            case 7: return "Pride";
+            default: return "Not a Sin";
+        }
     }
 }
